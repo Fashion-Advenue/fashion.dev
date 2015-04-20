3350054845aa5413fbd4e00606e47f102802abd2
Paula C Schuler <paulasatx@gmail.com>
2015-04-20 09:51:54 -0500

    Attempting to unify our files.

---
+++ sample_ads.php
@@ -0,0 +1,80 @@
+<?php 
+
+// an array of items for sale, simulation for "ads" database
+$items = [];
+
+$items = array(
+	'belt' => array(	
+		'headline' => 'Relic Silver Ball Grommet Belt',
+		'price' => '5.00',
+		'size' => 'large',
+		'category' => 'accessories',
+		'description'  => 'Synthetic leather with silver beading and nicely fashioned grommets.',
+		'contact' => 'text'
+		)
+	'shoe ornaments' => array(
+		'headline' => 'Fun Shoe Ornaments',
+		'price' => '10.00',
+		'size' => 'One Size',
+		'category' => 'shoes',
+		'description' => '3/4 inches long, various ornaments, total of 20 pieces',
+		'contact' => 'text'
+		)
+	'ring' => array(
+		'headline' => 'Diamond Ring, 14 carat gold',
+		'price' => '300.00',
+		'size' => '7',
+		'category' => 'jewelry',
+		'description' => 'Estate sale ring, appraised at $400 by Americus Diamond',
+		'contact' => 'text'
+		)
+	'necklace' => array(
+		'headline' => 'Heart and charm stones necklace',
+		'price' => '10.00',
+		'size' => '16 inches',
+		'category' => 'jewelry',
+		'description' => 'Never worn. Sterling silver with curbic zirconia',
+		'contact' => 'text'
+		)
+	'shoes' => array(
+		'headine' => 'Fun shoes for your little girl',
+		'price' => '10.00',
+		'size' => '7',
+		'category' => 'shoes',
+		'description' => 'Crafted by KB Designs, sure to please',
+		'contact' => 'text'
+		)
+	'dress' => array(
+		'headline' => 'Cute fun party or play dress.',
+		'price' => '20.00',
+		'size' => '2';
+		'category' => 'clothing',
+		'description' => 'Cute party dress or make believe dress! Top is cotton. Bottom is tulle, length 12 inches from waist.',
+		'contact' => 'text'
+		)
+	'jeans' => array(
+		'headline' => 'LA Idol USA jeans, good condition',
+		'price' => '18.00',
+		'size' => '11',
+		'category' => 'clothing';
+		'description' => 'Great jeans for a night out',
+		'contact' => 'text'
+		)
+	'purse' => array(
+		'headline' => 'Vera Bradley purse, blue flower design',
+		'price' => '20.00',
+		'size' => 'One Size',
+		'category' => 'accessories',
+		'description' => 'Shoulder bag, very nice condition',
+		'contact' => 'text'	
+		)
+	)
+
+
+
+
+
+
+
+
+ ?>
